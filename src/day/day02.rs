aec7796d0ff7b2badb920724e3a0ea70027b3b7f
Sergiovan <sergiovanpas@gmail.com>
2023-12-02 19:27:31 +0100

    Day 2 - Part 2

--- src/day/day02.rs
+++ src/day/day02.rs
@@ -48,4 +48,38 @@ impl Problem for Day2 {
     }).sum::<u64>().to_string()
   }
 
+  fn part2(&self, input: Vec<String>) -> String {
+    input.iter().map(|l| {
+      let mut iter = l.split(':');
+      let id = iter.next().unwrap().split(' ').last().unwrap().parse::<u64>().unwrap();
+      let games = iter.next().unwrap().split(';');
+      let minimum_cubes = games.map(|g| {
+        let mut cubes = Cubes{ red: 0, green: 0, blue: 0 };
+        
+        for cube in g.trim().split(',') {
+          let mut cube_parts = cube.trim().split(' ');
+          let num = cube_parts.next().unwrap().parse::<u64>().unwrap();
+          let color = cube_parts.next().unwrap().trim();
+
+          match color {
+            "red" => cubes.red += num,
+            "green" => cubes.green += num,
+            "blue" => cubes.blue += num,
+            _ => panic!("Invalid color: {}", color)
+          }
+        }
+
+        cubes
+      }).reduce(|l, r| {
+        Cubes {
+          red: l.red.max(r.red),
+          green: l.green.max(r.green),
+          blue: l.blue.max(r.blue)
+        }
+      }).unwrap();
+
+      minimum_cubes.red * minimum_cubes.green * minimum_cubes.blue
+
+    }).sum::<u64>().to_string()
+  }
 }
