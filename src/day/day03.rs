807e1d0fb74d1372e1df5405ea5fc78edeed2d6b
Sergiovan <sergiovanpas@gmail.com>
2023-12-03 17:39:25 +0100

    Day 3 - Part 2

--- src/day/day03.rs
+++ src/day/day03.rs
@@ -60,7 +60,60 @@ impl Problem for Day3 {
             })
           }
         }
-        // println!("{:?}", found);
+      }
+    }
+
+    adjacents.iter().sum::<u64>().to_string()
+  }
+
+  fn part2(&self, input: Vec<String>) -> String {
+    let bytes = input.iter().map(String::as_bytes).collect::<Vec<&[u8]>>();
+    let bytes = bytes.as_slice();
+
+    let mut first = None;
+    let mut last = (0, 0);
+    let mut buff = String::new();
+    let mut found = Vec::new();
+
+    for (y, line) in bytes.iter().enumerate() {
+      for (x, c) in line.iter().enumerate() {
+        match *c as char {
+          '0' ..= '9' => {
+            if first.is_none() {
+              first = Some((y as i32, x as i32));
+            }
+            last = (y as i32, x as i32);
+
+            buff.push(*c as char);
+          }
+          _ => {
+            if let Some(oneth) = first {
+              let ender = last;
+              found.push((buff.parse::<u64>().unwrap(), ((oneth.0 - 1, oneth.1 - 1), (ender.0 + 1, ender.1 + 1))));
+              buff = String::new();
+              first = None;
+              last = (0, 0);
+            }
+          }
+        }
+      }
+    }
+
+    let mut adjacents = Vec::new();
+
+    for (y, line) in bytes.iter().enumerate() {
+      for (x, c) in line.iter().enumerate() {
+        if *c as char == '*' {
+          let adjacent = found.iter().filter_map(|(n, area)| {
+            in_range(*area, (y, x)).then_some(*n)
+          }).enumerate().reduce(|(_, n), (idx, n2)| {
+            (idx, n * n2)
+          });
+
+          if let Some((1, n)) = adjacent {
+            adjacents.push(n);
+          }
+        }
       }
     }
 
